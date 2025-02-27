926719628ab64c09aee9a56982c6fc401d994fcd
Massami Ishioka <160499044+mcmassami@users.noreply.github.com>
2025-02-26 23:27:05 -0300

    feat: implementado uma função para agregar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,15 @@
 let amigos = [];  // Array inicializado para armazenar os nomes
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nome = input.value.trim();  // Captura o valor do input e remove espaços em branco
+
+    // Verifica se o nome é válido
+    if (!nome) {
+        alert('Digite um nome válido!');
+        return;
+    }
+
+    amigos.push(nome);  // Adiciona o nome ao array
+    input.value = '';  // Limpa o input
+}
